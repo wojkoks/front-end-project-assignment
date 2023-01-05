25c2d4dd21c422f045467d7f61cc13d1b5aaa647
Krysmal <malinowskizxcvbnm@gmail.com>
2023-01-05 12:00:32 +0100

    kalendarz

---
+++ kalendarz/CKalendarz.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import './CKalendarz.css';
+
+class CKalendarz extends Component
+{
+    render()
+    {
+
+    
+        return (
+            <div id="kal_body">
+                <div class="kal_week"><div class="kal_day kal_day_ntm"><div class="dn">2</div></div><div class="kal_day kal_day_ntm"><div class="dn">2</div></div><div class="kal_day kal_day_ntm"><div class="dn">2</div></div><div class="kal_day kal_day_ntm"><div class="dn">2</div></div><div class="kal_day kal_day_ntm"><div class="dn">2</div></div><div class="kal_day kal_day_ntm"><div class="dn">2</div></div><div class="kal_day_c"><div class="dn">2</div></div></div>
+                <div class="kal_week"><div class="kal_day kal_day_o"><div class="dn">2</div><div class="appo" onclick="">2 wolne<br/> terminy</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day_c"><div class="dn">2</div></div></div>
+                <div class="kal_week"><div class="kal_day kal_day_o"><div class="dn">2</div><div class="appo" onclick="">2 wolne<br/> terminy</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day_c"><div class="dn">2</div></div></div>
+                <div class="kal_week"><div class="kal_day kal_day_o"><div class="dn">2</div><div class="appo" onclick="">2 wolne<br/> terminy</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day_c"><div class="dn">2</div></div></div>
+                <div class="kal_week"><div class="kal_day kal_day_o"><div class="dn">2</div><div class="appo" onclick="">2 wolne<br/> terminy</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day"><div class="dn">2</div></div><div class="kal_day_c"><div class="dn">2</div></div></div>
+                <div class="kal_week"><div class="kal_day kal_day"><div class="dn">2</div></div><div class="kal_day kal_day"><div class="dn">2</div></div><div class="kal_day kal_day_ntm"><div class="dn">2</div></div><div class="kal_day kal_day_ntm"><div class="dn">2</div></div><div class="kal_day kal_day_ntm"><div class="dn">2</div></div><div class="kal_day kal_day_ntm"><div class="dn">2</div></div><div class="kal_day_c kal_day_ntm"><div class="dn">2</div></div></div>
+            </div>
+        );
+    }
+}
+
+
+export default CKalendarz;
