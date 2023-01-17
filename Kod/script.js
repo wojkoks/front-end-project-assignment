444edb437018b9076d039e7e77991e338f84f15a
wojkoks <wojtekkus123gmail.com>
2023-01-17 15:10:05 +0100

    rpawei

--- Kod/script.js
+++ Kod/script.js
@@ -1,15 +1,15 @@
 //let specbutton = document.getElementById('specbutton');
-let specbutton = document.getElementsByClassName('specbutton');
-for (let i = 0;i<specbutton.length;i++)
-{
-    specbutton[i].addEventListener("click",()=>{
-        alert("gitara siema");
-    })
-}
-let rezbutton = document.getElementsByClassName('rezbutton');
-for (let i = 0;i<rezbutton.length;i++)
-{
-    rezbutton[i].addEventListener("click",()=>{
-        alert("URRRRA");
-    })
-}
+// let specbutton = document.getElementsByClassName('specbutton');
+// for (let i = 0;i<specbutton.length;i++)
+// {
+//     specbutton[i].addEventListener("click",()=>{
+//         alert("gitara siema");
+//     })
+// }
+// let rezbutton = document.getElementsByClassName('rezbutton');
+// for (let i = 0;i<rezbutton.length;i++)
+// {
+//     rezbutton[i].addEventListener("click",()=>{
+//         alert("URRRRA");
+//     })
+// }
